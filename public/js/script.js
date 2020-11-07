5534727649cb2632ae6c7fe8d95d81b0b02a2373
Matias Ruiz <matiasruiz6899@gmail.com>
2020-11-07 14:08:22 -0300

    Animación de la X, no estoy conforme

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,21 @@
 var isComplete = true;
 
+//JQuery 
 $(document).ready(function () {
+
+  /*  En función del ancho de la pagina muestra o esconde el menu apenas cargue
+  *   seguro hay una mejor solucion, pero de momento funciona.
+  *   Tambien hay un error, simpre se muestra el menu en pantalla grande por un momento,
+  *   para luego desaparecer, queda feo y es muy molesto.
+  */  
+  var width = $(window).width();      
+  if (width <= 1024) {
+    $("#btnSlideDown").show();
+  } else {
+    $("#btnSlideDown").hide();
+  }
+
+  // Inicia el slider
   $("#autoWidth").lightSlider({
     autoWidth: true,
     loop: true,
@@ -9,6 +24,19 @@ $(document).ready(function () {
     },
   });
 
+
+  // En función del tamaño de ventana, aparece el menu o no
+  $(window).resize(function () {
+    var width = $(window).width();
+    if (width <= 1024) {
+      $("#btnSlideDown").show();
+    }
+    if (width >= 1024) {
+      $("#btnSlideDown").hide();
+    }
+  });
+
+  // Animación del menu
   $("#btnSlideDown").click(function () {
     if (isComplete) {
       $("#box").slideDown(500, function () {
@@ -21,6 +49,7 @@ $(document).ready(function () {
     }
   });
 
+  // Tranformación de la hamburguesa en una X
   $("#btnSlideDown").click(function () {
     $(".icon").toggleClass("close");
   });
