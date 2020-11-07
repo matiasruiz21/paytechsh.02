64bb4fdd806d0d60290f44944ba47712e38e331c
Matias Ruiz <matiasruiz6899@gmail.com>
2020-11-06 21:37:27 -0300

    slide menu y se arranco con la animacion de la x

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,27 @@
-// JavaScript Document
- $(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
+var isComplete = true;
+
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
   });
+
+  $("#btnSlideDown").click(function () {
+    if (isComplete) {
+      $("#box").slideDown(500, function () {
+        isComplete = false;
+      });
+    } else {
+      $("#box").slideUp(500, function () {
+        isComplete = true;
+      });
+    }
+  });
+
+  $("#btnSlideDown").click(function () {
+    $(".icon").toggleClass("close");
+  });
+});
